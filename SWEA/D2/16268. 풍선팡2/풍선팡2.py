ddfae7ca42e349d16a79cd56ec5896b533cc3d1c
ParkHeeJae <phj192331@gmail.com>
2025-03-19 00:49:48 +0900

    [D2] Title: 풍선팡2, Time: 95 ms, Memory: 60,160 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -0,0 +1,37 @@
+'''
+풍선 터트림 -> 상하 좌우 4방향으로 터짐
+상하좌우 델타 탐색 고려
+
+'''
+
+t=int(input())
+for tc in range(1, t+1):
+    # n : 행수, m : 열수
+    n, m = map(int, input().split())
+    lst = [list(map(int, input().split())) for _ in range(n)]
+  
+    # 상하좌우
+    dr = [-1, 1, 0 ,0] # 상하
+    dc = [0, 0, -1, 1] # 좌우
+
+    # 파아아앙아아아앙!
+    
+    max_pang = 0
+    for i in range(n):
+        for j in range(m):
+            sum_pang = lst[i][j]
+         
+            # 델타 탐색, 1~4 까지 돌면서 해당하는 값에 대해 적용
+            for k in range(4):
+                for l in range(1):
+                    # dr은 상하로만 이동, dc는 좌우로만 이동
+                    nr = i + dr[k]
+                    nc = j + dc[k]
+                    # 범위 설정
+                    if 0<=nr<n and 0<=nc<m:
+                        sum_pang += lst[nr][nc]
+            # 최대값을 갱신
+            if sum_pang > max_pang:
+                max_pang = sum_pang
+
+    print(f'#{tc} {max_pang}')
