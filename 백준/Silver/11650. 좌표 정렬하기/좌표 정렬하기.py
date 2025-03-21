8e9a51959776d87744337c143e1f92da4fd29561
ParkHeeJae <phj192331@gmail.com>
2025-03-21 11:57:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2848 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,13 @@
+'''
+2차원 평면 위의 점 N개가 주어진다.
+좌표를 x좌표가 증가하는 순으로, x좌표가 같으면
+y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+'''
+
+n = int(input())
+lst = [list(map(int, input().split())) for _ in range(n)]
+
+lst.sort(key=lambda x: (x[0], x[1]))
+
+for i in range(n):
+    print(lst[i][0], lst[i][1])
